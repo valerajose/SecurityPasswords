487685bcee3dd8fa798f7159ef28dc05112abcc2
valera09j <jvalera@legendsoft.com.ve>
2023-03-03 17:06:44 -0400

    logica para generar contraseña

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 
+const lengthSlider = $(".pass-length input")[0];
+// const options = $(".option input")[0];
+const options = document.querySelectorAll(".option input");
+const copyIcon = $(".input-box span");
+const passwordInput = $(".input-box input");
+const passIndicator = $(".pass-indicator");
+const generateBtn = $(".generate-btn");
+
+
+const characters = {
+    lowercase : "abcdefghijklmnopqrstuvwxyz",
+    uppercase : "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    numbers : "0123456789",
+    symbols : "!$·&()=?¿'¡*{}"
+}
+
+console.log(options);
+
+const generatePassword = () =>{
+
+    let staticPassword = "",
+    randomPassword = "",
+    excludeDuplicate = false,
+    passLenght = lengthSlider
+   
+    options.forEach(option => {
+        
+        if(option.checked){
+            if(option.id !== "exc-duplicate" && option.id !== "spaces"){
+                staticPassword += characters[option.id];
+                console.log(staticPassword);
+            }
+        }
+
+    });
+    
+}
+
+
+
